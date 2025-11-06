17da11bb2459956eb238bab56e186eb3ac073656
Seetal <gudufb1@gmail.com>
2025-11-06 08:28:55 +0530

    JPA Annotation

--- src/main/java/com/seetal/hibernatedemo/HibernatedemoApplication.java
+++ src/main/java/com/seetal/hibernatedemo/HibernatedemoApplication.java
@@ -1,13 +1,22 @@
 package com.seetal.hibernatedemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernatedemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernatedemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner->{
+			System.out.print("!!!!!!!!!!!!!!!!!!!!!!Hello World!!!!!!!!!!!!!!!!!!HIBERNATE!!!!!!!!!!");
+		};
 	}
 
 }
---
+++ src/main/java/com/seetal/hibernatedemo/entity/Student.java
@@ -0,0 +1,76 @@
+package com.seetal.hibernatedemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private int id;
+
+    @Column(name="first_name")
+    private String firstname;
+
+    @Column(name="last_name")
+    private String lastname;
+
+    @Column(name="email")
+    private String email;
+
+    //constructors
+    public Student() {
+    }
+
+    public Student(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    //getter setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    //tostring
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
